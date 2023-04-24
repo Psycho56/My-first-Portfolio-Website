1d1616a535553c6a3b8d98a327620312c281c71d
Psycho56 <kuliyevkamoliddin5@gmail.com>
2023-04-23 18:31:15 -0600

    changed

--- src/utils/lazy-loading.js
+++ src/utils/lazy-loading.js
@@ -5,7 +5,7 @@ const lazyLoading = () => {
      entries.forEach(entry => {
       if (entry.isIntersecting) {
         let img = entry.target;
-        img.src = img.dataset.src;
+        // img.src = img.dataset.src;
         img.classList.remove('loading');
         img.classList.add('loaded');
          observer.unobserve(entry.target);
